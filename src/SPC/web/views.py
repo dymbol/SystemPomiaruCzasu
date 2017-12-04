c99cf65d828a03d0e0543b9ad218c1613f588ee8
Bartosz Dempc <dempc.bartosz@gmail.com>
2017-12-04 14:45:19 +0100

    first page almost work (index.html)

--- src/SPC/web/views.py
+++ src/SPC/web/views.py
@@ -3,17 +3,10 @@ from django.contrib.auth.decorators import login_required
 from web.models import *
 
 # Create your views here.
-@login_required(login_url='login_user')
+
+
 def index(request):
     context = {}
-    races = []
-
-    if request.user.groups.values_list('name', flat=True).count() == 0:
-        #user doesn't belong to any group
-        print("User {} doesn't belong to any group".format(request.user.username))
-    else:
-        #get albums list for that user
-        races = Race.objects.all().dates('creation_date', 'year').distinct()
-        context["races"] = races
+    context["races"] = Race.objects.all() #.dates('creation_date', 'year').distinct()
 
     return render(request, 'index.html', context)
