bf1a2a9c77d29aff142325c66b1afb9cca797678
Bartosz Dempc <dempc.bartosz@gmail.com>
2018-02-09 14:19:49 +0100

    pep8 error fix /src/SPC/web/tinydb_con.py:3:47: W292 no newline at end of file
    ./src/SPC/web/apps.py:8:27: W292 no newline at end of file
    
    ./src/SPC/web/admin.py:10:27: W292 no newline at end of file
    
    ./src/SPC/web/context_processors.py:2:5: E265 block comment should start
    with '# '

--- src/SPC/web/context_processors.py
+++ src/SPC/web/context_processors.py
@@ -1,5 +1,5 @@
 def chosen_race(request):
-    #Context processor
     if request.user.is_authenticated and request.user.is_active:
         if 'chosen_race' in request.session.keys():
             return {'CHOSEN_RACE': request.session['chosen_race']}
+
