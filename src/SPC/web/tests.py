8df7a1ca41b95271bdb478bd23653caac74df0ec
Bartosz Dempc <dempc.bartosz@gmail.com>
2018-02-09 14:46:16 +0100

    creating first test

--- src/SPC/web/tests.py
+++ src/SPC/web/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class TestSites(TestCase):
+    '''Testing if subsites works'''
+    def setUp(self):
+        user = User.objects.create_user('temporary', 'tmp@tmp.pl', 'temporary')
+    def test_secure_page(self):
+        print("======= BASIC SITE TESTING MODULE =======")
+        self.client.login(username="temporary", password="temporary")
+        sites = ['',
+                 'race/choose/1',
+                 'team/list',
+                 'results',
+                 'result/register',
+                 'change_track/1',
+                 'time_meter/1',
+                 ]
+        for site in sites:
+            print("Testing '{}' site".format(site))
+            response = self.client.get('/web/{}'.format(site), follow=True)
+            self.assertEqual(response.status_code, 200)
